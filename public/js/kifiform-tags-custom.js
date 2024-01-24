92bf1733b36be658280f4cdb5d1291843b5aaf71
Elias Tertsunen <elias.tertsunen@kirjastot.fi>
2024-01-24 12:54:03 +0000

    Redirect user by using from submit
    - hook_user_login can no longer be used for redirecting, because then normal login then fails to finalize login operation leaving user unlogged.
    - Remove a left over console.log from kifiform-tags-custom.js file.

--- public/js/kifiform-tags-custom.js
+++ public/js/kifiform-tags-custom.js
@@ -3,7 +3,6 @@
 
   $(once("asklib-tag-insert", "form.asklib-question-edit-form input.form-autocomplete"))
     .on("kififormtaginsert", function(event, ui) {
-      console.log(event);
 
       if (ui.item.autocompleted == true) {
         // This is valid as long autocomplete is configured to match only Finto terms.
